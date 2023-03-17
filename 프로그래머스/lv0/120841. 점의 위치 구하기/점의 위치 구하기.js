1dbc344b783e98a2b563793ed7be3436d2cae744
조성원 <77275383+thejsw@users.noreply.github.com>
2023-03-17 14:50:28 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,14 +1,14 @@
 function solution(dot) {
-    if (dot[0] > 0) {
-        if (dot[1] > 0) {
-            return 1
-        }
-        return 4
-    }
-    else {
-        if (dot[1] > 0) {
-            return 2
-        }
-        return 3;
-    }
+    return (dot[0] > 0) ? 
+        (dot[1] > 0) ? 1 : 4 
+    : (dot[1] > 0) ? 2 : 3;
 }
+    
+//     if (dot[0] > 0) {
+//         if (dot[1] > 0) {return 1}
+//         return 4
+//     }
+//     else {
+//         if (dot[1] > 0) {return 2}
+//         return 3;
+//     }
