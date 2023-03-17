debe5e7753765f001e62802bf5ea47932117953c
조성원 <77275383+thejsw@users.noreply.github.com>
2023-03-17 20:33:11 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,3 +1,14 @@
 function solution(s1, s2) {
     return intersection = s1.filter((v) => s2.includes(v)).length;
 }
+
+// function solution(s1, s2) {
+//     let cnt = 0
+//     for (let i of s1) {
+//         for (let j of s2) {
+//             if (i === j) {
+//                 cnt++
+//             }}
+//     }
+//     return cnt
+// }
