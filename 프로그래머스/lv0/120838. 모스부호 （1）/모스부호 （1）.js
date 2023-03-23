57ea449e58c24f927c390047fd2e792a88e25b3e
조성원 <77275383+thejsw@users.noreply.github.com>
2023-03-23 14:55:33 +0900

    [level 0] Title: 모스부호 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,14 @@
+function solution(letter) {
+    let morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    
+    let newLetter = letter.split(' ')
+    let result = ''
+    newLetter.map(v => result += morse[v])
+    return result
+}
