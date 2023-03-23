3684705fc0fc6ab2caef40f2a89a9057558f02d4
조성원 <77275383+thejsw@users.noreply.github.com>
2023-03-23 14:41:20 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,14 @@
+function solution(age) {
+    let newArr = String(age).split('')
+    .map(v => Number(v) + 97)
+    .map(v => String.fromCodePoint(v))
+    
+    let result = ''
+    newArr.map(v => result += v)
+    return result
+}
+
+// 한 자리씩 나누기 V
+// +97 V
+// 아스키코드로 바꾸기 V
+// 합치기
