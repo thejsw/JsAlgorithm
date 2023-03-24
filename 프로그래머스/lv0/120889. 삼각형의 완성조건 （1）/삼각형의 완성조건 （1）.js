c0e76d98989daf25f36514b8f3643d8243c35f59
조성원 <77275383+thejsw@users.noreply.github.com>
2023-03-24 20:41:05 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,10 +1,17 @@
 function solution(sides) {
   sides = sides.sort((a, b) => b - a);
-  return sides[0] < sides[1] + sides[2] ? 1 : 2;
-
+  return (sides[0] < sides[1] + sides[2]) ? 1 : 2;
 }
+
+// 입력: 배열 > 선분 세 개 [1, 2, 3]
+// 1. 가장 큰 값을 정한다 > sort로 정렬하기 > 배열 [0] 가장 큰 값이
+// 2. (가장 큰 값이 나머지 둘의 합보다) 작으면 1 아니면 2
+// 출력: 1 또는 2
+
+
 // function solution(sides) {
 //     let max = Math.max(...sides);
-//     let newArr = sides.filter(v => v !== max);
+//     let newArr = sides.filter(v => v !== max && sides.length > 2);
+//     // return newArr
 //     return (max < newArr[0] + newArr[1]) ? 1 : 2;
 // }
