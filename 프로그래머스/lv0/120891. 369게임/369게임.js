8204c0741b90a313bc78570c40f23a14dc242501
조성원 <77275383+thejsw@users.noreply.github.com>
2023-03-24 19:56:06 +0900

    [level 0] Title: 369게임, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,4 +1,11 @@
+// function solution(order) {
+//     let newArr = String(order).split('')
+//     return newArr.filter(v => v === "3" || v === "6" || v === "9").length
+// }
+
 function solution(order) {
+    let cnt = 0
     let newArr = String(order).split('')
-    return newArr.filter(v => v === "3" || v === "6" || v === "9").length
+    newArr.map(v => { if (v % 3 === 0 && v != 0) { cnt ++ }})
+    return cnt;
 }
