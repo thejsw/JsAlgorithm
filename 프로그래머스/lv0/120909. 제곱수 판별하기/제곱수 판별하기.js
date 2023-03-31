830d602f2233d777b0a9da03f61c96c494bf2740
조성원 <77275383+thejsw@users.noreply.github.com>
2023-03-31 20:24:38 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,4 +1,6 @@
 function solution(n) {
-    return (n === 1) ? 2
-    : (Number.isInteger(Math.sqrt(n))) ? 1 : 2
+    return (Number.isInteger(Math.sqrt(n))) ? 1 : 2
 }
+
+// 1. 1제거 > 굳이 안해도 되더라
+// 2. 루트 n이 정수면 1 아니면 2
