47ef93c83d05cff9c9320854a039782b24b80dba
조성원 <77275383+thejsw@users.noreply.github.com>
2023-04-06 17:04:25 +0900

    [level 0] Title: 개미 군단, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -4,6 +4,8 @@ function solution(hp) {
     return [a, b, c].reduce((arr,cur) => arr += cur, 0)
 }
 
+// return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + Math.floor((hp % 5) % 3)
+
 // 몫만 리턴
 // 1. 5로 나눈 몫 저장
 // 2. 3으로 나눈 몫 저장
