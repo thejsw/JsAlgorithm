a34f959e630229fc29836618223c4e102c5b3636
조성원 <77275383+thejsw@users.noreply.github.com>
2023-04-10 17:49:03 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,7 +1,9 @@
 function solution(num_list) {
-    let result = []
-    for (let i=num_list.length-1; i>=0; i--) {
-        result.push(num_list[i])
-    }
+    return num_list.sort((a, b) => -1);
     return result
 }
+
+// let result = []
+//     for (let i=num_list.length-1; i>=0; i--) {
+//         result.push(num_list[i])
+//     }
