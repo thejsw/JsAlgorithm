49fb3781e64b63c5a6d9b87925758796be9d1ced
조성원 <77275383+thejsw@users.noreply.github.com>
2023-04-23 17:37:35 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,16 +1,3 @@
 function solution(array) {
-    let cnt = 0
-    array.join().split('').forEach((el) => {
-        if (el === '7') {
-            cnt++
-        }
-    })
-    return cnt
-    
-    //     .reduce((acc, cur) => {
-    //     if (array.includes('7')) {
-    //         acc++
-    //     }
-    // }, 0)
-    // return acc
+    return array.join().split('').filter((el) => el === '7').length
 }
