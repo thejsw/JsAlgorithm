5ffcf12b5a75af01c11d6bea9b0e4d0bbf961c64
조성원 <77275383+thejsw@users.noreply.github.com>
2023-05-22 17:05:56 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,13 +1,12 @@
 function solution(number) {
-    var answer = 0;
-    for(var i=0; i<number.length; i++){
-        for(var j=i+1; j<number.length; j++){
-            for(var k=j+1; k<number.length; k++){
-                if(number[i] + number[j] + number[k] === 0){
-                    answer += 1;
-                }
+    let result = 0;
+    for(let i = 0, length = number.length; i < length; i++) {
+        for(let j = i+1; j < length; j++) {
+            for(let k = j+1; k < length; k++) {
+                if(number[i]+number[j]+number[k]) continue;
+                result++;
             }
         }
     }
-    return answer;
+    return result;
 }
